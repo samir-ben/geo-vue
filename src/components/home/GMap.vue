7e1a7c24c69058087c2247241f189b3bb5dbda8b
samir-ben <samir-69@hotmail.fr>
2019-01-12 15:14:13 +0100

    feat: google map
    - add google map script in index
    - config google map in gmap component

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="map">
-    map
+    <div class="google-map" id="map"></div>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+import db from '@/firebase/init'
 export default {
   name: 'GMap',
-  data () {
-    return {
+  data(){
+    return{
+      lat: 53,
+      lng: -2
     }
+  },
+  methods: {
+    renderMap(){
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: this.lat, lng: this.lng },
+        zoom: 6,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false
+      });
+    }
+  },
+  mounted(){
+    this.renderMap()
   }
 }
 </script>
 
-
-<style scoped>
+<style>
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
 </style>
