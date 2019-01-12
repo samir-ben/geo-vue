8305ff19b522e322994ee1442d07378ab5dd555c
samir-ben <samir-69@hotmail.fr>
2019-01-12 16:42:29 +0100

    feat: navbar routerlinks
    - add routerlinks in navbar component
    - add logout method in navbar component
    - created login component
    - add form in login component
    - add empty login method in login component
    - config router (add login route)

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="login container">
+    <form class="card-panel" @submit.prevent="login">
+      <h2 class="center deep-purple-text">Connexion</h2>
+      <div class="field">
+        <label for="email">Email</label>
+        <input id="email" type="email" v-model="email">
+      </div>
+      <div class="field">
+        <label for="password">Mot de passe</label>
+        <input id="password" type="password" v-model="password">
+      </div>
+      <p v-if="feedback" class="red-text center">{{ feedback }}</p>
+      <div class="field center">
+        <button class="btn deep-purple">Se connecter</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+  name: 'Login',
+  data(){
+    return{
+      email: null,
+      password: null,
+      feedback: null
+    }
+  },
+  methods: {
+    login(){
+    }
+  }
+}
+</script>
+
+<style>
+.login{
+  max-width: 400px;
+  margin-top: 60px;
+}
+.login h2{
+  font-size: 2.4em;
+}
+.login .field{
+  margin-bottom: 16px;
+}
+</style>
