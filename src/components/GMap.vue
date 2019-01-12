b37b92d774d9f7daa129028573193a8848837b89
samir-ben <samir-69@hotmail.fr>
2019-01-12 14:15:43 +0100

    feat: clean app
    - delete helloworld component
    - add gmap component
    - delete app root style
    - add cdn materialize css
    - add cdn google font icon

---
+++ src/components/GMap.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="map">
+    map
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GMap',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+</style>
