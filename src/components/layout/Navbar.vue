e3525f9cf70c7e63855ab3dc83e469bf70a33fac
samir-ben <samir-69@hotmail.fr>
2019-01-12 14:26:42 +0100

    feat: navbar
    - created layout folder in component
    - created nabvar component
    - add a materialize navbar template html
    - put this navbar in app vue

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <div class="container">
+        <a href="#" class="brand-logo">GeoVue</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a href="sass.html">S'inscrire</a>
+          </li>
+          <li>
+            <a href="badges.html">Se connecter</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
