a0deb600488f1f26fbade535bb2e9f40835d4698
samir-ben <samir-69@hotmail.fr>
2019-01-16 13:52:44 +0100

    feat: conditionnal links
    - add user variable aquel to null
    - add condition in li tags
    - add a method in created hook to get state user and change user var

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -4,9 +4,10 @@
       <div class="container">
         <router-link class="brand-logo" :to="{ name: 'GMap' }">GeoVue</router-link>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><router-link :to="{ name: 'Signup' }">Inscription</router-link></li>
-          <li><router-link :to="{ name: 'Login' }">Connexion</router-link></li>
-          <li><a @click="logout">Se déconnecter</a></li>
+          <li v-if="!user"><router-link :to="{ name: 'Signup' }">Inscription</router-link></li>
+          <li v-if="!user"><router-link :to="{ name: 'Login' }">Connexion</router-link></li>
+          <li v-if="user"><a>{{ user.email }}</a></li>
+          <li v-if="user"><a @click="logout">Se déconnecter</a></li>
         </ul>
       </div>
     </div>
@@ -19,7 +20,19 @@ import firebase from 'firebase'
 export default {
   name: "Navbar",
   data() {
-    return {};
+    return {
+      user: null
+    };
+  },
+  created() {
+    // let user = firebase.auth().currentUser
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.user = user
+      } else {
+        this.user = null
+      }
+    })
   },
   methods: {
       logout(){
